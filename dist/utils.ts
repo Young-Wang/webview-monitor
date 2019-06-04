4c908873eb46c0dba94870cc85c23b10068808eb
Young <holywangyong@hotmail.com>
2019-06-04 11:28:36 +0800

    Build project

---
+++ dist/utils.ts
@@ -0,0 +1,12 @@
+const time = {
+  format(date: Date): string {
+    const year = date.getFullYear();
+    const month = date.getMonth() + 1;
+    const day = date.getDate();
+    const hour = date.getHours();
+    const min = date.getMinutes();
+    const sec = date.getSeconds();
+    return `${year}-${month}-${day} ${hour}:${min}:${sec}`;
+  }
+}
+export { time };
